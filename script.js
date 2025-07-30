c78897f65d471d5aceb206b7e2ee45c6c34e3452
danabol100 <126118288+danabol100@users.noreply.github.com>
2025-07-30 13:10:20 +0300

    secrion look

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ close.addEventListener('click', () => {
     menu.classList.add('hidden');
 
 })
+
+
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const headerOffset = 80; // высота твоего фиксированного хедера
+        const elementPosition = target.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
+
